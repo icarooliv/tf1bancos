de21f0c7eb46de9a6a1c2b5cd82075e8d9c95d08
Lucas Machado <lucasmmsilva@gmail.com>
2018-12-03 15:13:13 -0200

    adds file APAGA

---
+++ TF1_APAGA.sql
@@ -0,0 +1,43 @@
+
+-- ---------------------------- TF1 aval -------------------------- --
+--                                                                  --
+--                    SCRIPT DE APAGAR (DDL)                        --
+--                                                                  --
+-- Data Criacao ..........: 03/12/2018                              --
+-- Autor(es) .............: Lucas Machado de Moura e Silva          --
+--                          Ícaro Oliveira                          --
+-- Banco de Dados ........: MySQL                                   --
+-- Base de Dados(nome) ...: homecare                                --
+--                                                                  --
+-- Data Ultima Alteracao ..:                                        --
+--                                                                  --
+-- PROJETO => 1  Base de Dados                                      --
+--      => 22 Tabelas                                               --
+--                                                                  --
+-- ---------------------------------------------------------------- --
+
+USE homecare; 
+
+DROP TABLE TECNICO_EQUIPAMENTO;
+DROP TABLE PACIENTE_EQUIPAMENTO;
+DROP TABLE TECNICO_COOPERATIVA;
+DROP TABLE PACIENTE_PATOLOGIA;
+DROP TABLE AVALIACAO;
+DROP TABLE TELEFONECOOPERATIVA;
+DROP TABLE TELEFONEPESSOA;
+DROP TABLE EQUIPAMENTO;
+DROP TABLE EVOLUCAO;
+DROP TABLE PRESCRICAO;
+DROP TABLE MEDICAMENTO;
+DROP TABLE VISITA;
+DROP TABLE TIPOVISITA;
+DROP TABLE TECNICO;
+DROP TABLE COOPERATIVA;
+DROP TABLE PROFISSIONAL;
+DROP TABLE CARGO;
+DROP TABLE CATEGORIAPROFISSIONAL;
+DROP TABLE PACIENTE;
+DROP TABLE PESSOA;
+DROP TABLE PATOLOGIA;
+DROP TABLE ESTADOCIVIL;
+DROP TABLE COMPLEXIDADE;
